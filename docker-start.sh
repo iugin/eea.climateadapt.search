48569f92495398ce55d81b1e1bb1e29f389a6708
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2018-03-14 12:00:08 +0200

    Added docker files

---
+++ docker-start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+if [[ "${DEV_ENV:-false}" == "true" ]] ; then
+    cd /code/config
+
+    if [ -d ".git" ]; then
+       git pull
+    else
+       git clone $github_repo . --depth=5
+    fi
+    #on commit ignore the permission changes
+    git config core.filemode false
+
+    #permissions are needed because the nodejs app needs to be able to
+    #create new folders and copy existing ones
+    chmod 777 /code/config
+    chmod -R 775 /code/config/*
+else
+    #if production mode, use the configs that are inside the image
+    echo "222"
+    cp /config -R /code/
+fi
