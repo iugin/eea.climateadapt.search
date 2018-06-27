a7e6a0fb374cd0ae2a7d08f3382877c291a0385b
Adrian Apostol <dev.adrianapostol@gmail.com>
2018-06-27 16:27:35 +0300

    Refs #90275 - created wise config folder

---
+++ wise/public/javascripts/climateadapt.facetview.js
@@ -0,0 +1,101 @@
+window.esbootstrap_options = {
+    search_sortby: [
+        {
+            'field': 'title',
+            'display_asc': 'Title a-z',
+            'display_desc': 'Title z-a'
+        },
+        {
+            'field': 'issued',
+            'display_asc': 'Oldest',
+            'display_desc': 'Newest'
+        }
+    ],
+    sort: [{'issued': {'order': 'desc'}}],
+    paging: {
+        from: 0,
+        size: 50
+    },
+    display_type: 'card',
+    resultModifier: updateResult
+};
+function updateContentTypes(element, result){
+    result.contentType = 'generic';
+    var contentTypes = {};
+
+    if (eea_mapping.types !== undefined){
+        if (eea_mapping.types.defaultContentType !== undefined){
+            result.contentType = eea_mapping.types.defaultContentType;
+        }
+        if (eea_mapping.types.contentTypeNormalize !== undefined){
+            contentTypes = eea_mapping.types.contentTypeNormalize;
+        }
+    }
+
+    if(result.type === undefined) {
+        result.type = '';
+    }
+    result.types = result.type;
+    result.typeClass = '';
+    if (!$.isArray(result.types)){
+        result.types = [result.types];
+    }
+
+    if (result.types.length>0) {
+        var pos = result.types.length - 1;
+        while(true){
+            result.type = result.types[pos];
+            result.typeClass = result.type.toLowerCase().replace(/\s/g, '-');
+            if (contentTypes[result.typeClass]){
+                result.contentType = contentTypes[result.typeClass];
+                break;
+            }
+            pos--;
+            if (pos < 0){
+                break;
+            }
+        }
+    }
+    if ((eea_mapping.types !== undefined) && (eea_mapping.types.images !== undefined) && (eea_mapping.types.images.rules !== undefined)){
+        if (eea_mapping.types.images.fallback_thumb !== undefined){
+            result.thumbUrl = eea_mapping.types.images.fallback_thumb;
+        }
+        if (eea_mapping.types.images.fallback_icon !== undefined){
+            result.typeIcon = eea_mapping.types.images.fallback_icon;
+        }
+        var found_rule_for_resource = false;
+        for (var rule_count = 0; rule_count < eea_mapping.types.images.rules.length; rule_count++){
+            if (!found_rule_for_resource){
+                var rule = eea_mapping.types.images.rules[rule_count];
+                var field_for_rule = simpleValue(element[rule.field]);
+                if (field_for_rule === undefined){
+                    field_for_rule = "";
+                }
+                var compare_result = false;
+                if (rule.rule === 'startsWith'){
+                    compare_result = (field_for_rule.startsWith(rule.value));
+                }
+                if (rule.rule === 'contains'){
+                    if (field_for_rule.indexOf(rule.value) !== -1){
+                        compare_result = true;
+                    }
+                }
+                if (rule.operator === 'not'){
+                    compare_result = !compare_result;
+                }
+                found_rule_for_resource = compare_result;
+                if (found_rule_for_resource){
+                    result.thumbUrl = get_image("thumb_template", element, result, rule);
+                    result.typeIcon = get_image("icon_template", element, result, rule);
+                }
+            }
+        }
+    }
+    return(result);
+}
+
+function updateResult(element, result){
+    result = updateContentTypes(element, result);
+    return(result);
+}
+
