30796be7f33b7387e281043ece7a632b3b6bf5f3
Adrian Apostol <dev.adrianapostol@gmail.com>
2018-06-29 13:35:18 +0300

    Refs #90275 - wise fix

--- wise/public/javascripts/wise.facetview.js
+++ wise/public/javascripts/wise.facetview.js
@@ -1,7 +1,7 @@
 window.esbootstrap_options = {
     search_sortby: [
         {
-            'field': 'label',
+            'field': 'title.eea_title',
             'display_asc': 'Title a-z',
             'display_desc': 'Title z-a'
         },
